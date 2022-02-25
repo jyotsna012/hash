3339068c2193878ef4a6d8bd3857869978d379df
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-24 21:36:29 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,4 +39,53 @@ void hashtable(int ntableSize){
   }
 }
 
+int Hash(char* key){
+  int hash = 0;
+  int index;
+  
+  index = strlen(key);
+  
+  for(int i =0; i < strlen(key); i++){
+    hash = hash + (int)key[i];
+  }
+  
+  index = hash % tableSize;
+  return index;
+  
+}
+
+void addStudent(char* firstName, char* lastName, int studentId, float GPA){
+  
+  int index = Hash(firstName);
+  if(HashTable[index] -> studentId == 0){
+    strcpy(HashTable[index]->firstName, firstName);  
+    strcpy(HashTable[index]->lastName, lastName);  
+    HashTable[index] -> studentId = studentId;
+    HashTable[index] -> GPA = GPA;
+    HashTable[index] -> next = NULL;
+
+  }
+  else{
+  
+    Student* ptr = HashTable[index];
+    Student* n = new Student;
+    strcpy(n->firstName, firstName);  
+    strcpy(n->lastName, lastName);  
+    n -> studentId = studentId;
+    n -> GPA = GPA;
+    n->next = NULL;
+    while(ptr -> next != NULL){
+      ptr = ptr -> next;
+    }
+    
+    ptr -> next = n;
+  
+  }
+  
+  if(numItemsIndex(index) >= 4){
+
+    cout << "needs to be rehased" << endl;
+  }
+}
+
 
